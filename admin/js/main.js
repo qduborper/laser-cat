06e3970ba9be99e70b060575281f98d2442b676f
qduborper <qduborper@macbook-pro-de-quentin.home>
2015-10-10 15:18:08 +0200

    Integrate calibration directly in admin

--- admin/js/main.js
+++ admin/js/main.js
@@ -43,4 +43,70 @@
         socket.emit('setBreaks', $('.js-breaks').val());
     });
 
+
+    // Calibration
+
+    // Setup servo controls
+    var setupAxis = function(axis) {
+        var group = $('#' + axis);
+        var input = group.find('input');
+        var erroralert = group.find('div.alert-danger');
+        var updateAxis = function() {
+            socket.emit('updateAxis', { axis : axis, val : input.val() }, function(){
+                group.removeClass('has-error');
+                erroralert.hide();
+            });
+        };
+        erroralert.hide();
+        input.change(function() {
+            updateAxis();
+        });
+        group.find('button.decrease').click(function() {
+            input.val(Number(input.val()) - 2);
+            updateAxis();
+        });
+        group.find('button.increase').click(function() {
+            input.val(Number(input.val()) + 2);
+            updateAxis();
+        });
+    };
+    setupAxis('xaxis');
+    setupAxis('yaxis');
+    // Update servo positions by querying server
+    var updateServos = function() {
+        socket.emit('getLaser', function(data){
+            console.log('get laser', data);
+            $('#xaxis input').val(data.x);
+            $('#yaxis input').val(data.y);
+        });
+    };
+    
+    // Generate an event that fires when the video image is loaded.
+    // Use both the load event on the image, and a 1 second timeout as
+    // a fallback.  I found the fallback is necessary with Chrome 
+    // because it sometimes never fires the load event for an MJPEG image.
+    var video = $('#video img').first();
+    var videoLoad = $.Deferred();
+    window.setTimeout(function() {
+        videoLoad.resolve();
+    }, 1000);
+    video.load(function() {
+        videoLoad.resolve();
+    });
+    
+    // Wait for the video image to load, then setup calibration.
+    videoLoad.done(function() {
+        calibration.setup('calibrateLayer', video.width(), video.height());
+        updateServos();
+    });
+    // Send target commands based on click locations
+    $('#calibrateLayer').click(function(ev) {
+        if (!calibration.isCalibrating()) {
+            socket.emit('laserMove', { x: ev.offsetX, y: ev.offsetY }, function(){
+                updateServos();
+            });
+        }
+    });
+
 })(jQuery);
+

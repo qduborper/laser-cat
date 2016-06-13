19615de6fbe60983d9cc6dd1ca646cbe104f14d1
qduborper <qduborper@wideagency.com>
2016-06-13 23:09:33 +0200

    Invert rasp pins

--- server/server.js
+++ server/server.js
@@ -62,7 +62,7 @@ board.on("ready", function() {
             minPos: 0,
             maxPos: 180,
             offset: 45,
-            pin: 'P1-12',
+            pin: 'P1-32',
             range: [0,180],
             invert: false
         }),
@@ -70,7 +70,7 @@ board.on("ready", function() {
             minPos: 0,
             maxPos: 45,
             offset: 40,
-            pin: 'P1-32',
+            pin: 'P1-12',
             range: [0,45],
             invert: false
         }),
